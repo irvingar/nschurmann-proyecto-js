ccd1719e44cd91b4f1f5422bc2aea29f359c5270
irving garcia <irvingmartirgar@gmail.com>
2023-08-01 17:56:39 -0600

    regresando los archivos

---
+++ index.js
@@ -0,0 +1,15 @@
+const fn = () => {
+    const miString = "soy un string!"
+    let b = miString + "muy feliz"
+    console.log(b)
+    console.log('hola Mundo');
+    
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
